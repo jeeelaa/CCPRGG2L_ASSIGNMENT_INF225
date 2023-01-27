c22d2cb78ebd45a934e3aee41ae610ce9e8aab79
Anjiela Vejar <120693848+jeeelaa@users.noreply.github.com>
2023-01-27 11:00:39 +0800

    Create Person.java

---
+++ Exercise_4/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+
+    // attributes for App 
+    String name;
+    int age;
+
+    //constructor
+    Person (String personName, int personAge){
+        this.age=personAge;
+        this.name=personName;
+    }
+    
+
+}
